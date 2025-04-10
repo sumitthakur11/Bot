1734c901a131d49391c21eb9475bedf95e0288d9
sumitthakur11 <sumitsthakur1111@gmail.com>
2025-04-11 01:08:26 +0530

    backtest improved

--- Backtest/backtest.py
+++ Backtest/backtest.py
@@ -1,32 +1,46 @@
 
 from Bot import env
-from Strategy import bb as strategy
-from utility import utility
+from Bot.Strategy import bb as strategy
+from Bot.utility import utility
+
 import logging
 import os 
 import sys
-
+import pandas as pd 
 path= env.currenenv
-logpath= os.path.join(path,'botlogs/Angelbroker.logs')
+logpath= os.path.join(path,'botlogs/backtest.logs')
 logpath= os.path.normpath(logpath)
 # logpath= os.path.join(logpath,'Angelbroker.logs')
 print(logpath,'logpath')
 logger=env.setup_logger(logpath)
 
+retryno= 0
 
 def scheduelbacktest():
     try:
-        retryno= 0
         misc= utility.misc()
         symbollist=misc.getsymbols()
+        print(symbollist)
+        datalist= []
+        stat=strategy.strategy()
         
-        for i in symbollist:
+        for i in symbollist['symbol']:
             logger.info(f"backtest starts for symbol:{i}")
-            data = misc.getdata(i)
-            for j in data.items():
-                flag=strategy.strategy.finalprocess(j,True)
-                logger.info(f"current running ticks are as follows:{j}")
-                passed= "strategy runnning well" if flag else "something went wrong.check detail in strategy executions logs"
+            data = misc.getdata(i,test=True)
+            for j in range(len(data)):
+                datadict={}
+                datadict['updated_at']= data['updated_at'].iloc[j]
+                datadict['Close']= data['Close'].iloc[j]
+                datadict['OI']= data['OI'].iloc[j]
+                datadict['Volume']= data['Volume'].iloc[j]
+                datalist.append(datadict)
+                datafin= pd.DataFrame(datalist)
+                datafin= misc.buildcandels(datafin,'5min',True)
+                print(float(data['Close'].iloc[j]))
+                flag=stat.main(datafin,True)
+                misc.checkpnlbox(float(data['Close'].iloc[j]))
+                misc.closeorder()
+                passed= "strategy runnning well" if {flag} else "something went wrong.check detail in strategy executions logs"
                 logger.debug(passed)
         logger.info(f"backtest completed for symbol:{i}")
         
@@ -43,18 +57,25 @@ def scheduelbacktest():
 
 
     except Exception as e :
+        global retryno
         retryno+=1
-        logger.error(f"Got an exception {e}")
+        print(retryno)
+        logger.error(f"Got an exception {e}",exc_info=True)
         logger.info(f"restart function again retry:{retryno}")
-        scheduelbacktest()
         if retryno>3:
+        
             logger.info(f"retry exceeded max retry allowed ")
             sys.exit(1)
+
+            retryno= 0
+
+        scheduelbacktest()
 
         
 
 
 
+scheduelbacktest()
 
 if __name__ =="__main":
     scheduelbacktest()
